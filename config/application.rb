973d25afe507b94007fa18f610f93b78a9665e38
Paul Buzuloiu <pbuzu025@uottawa.ca>
2019-01-19 01:22:00 -0500

    faker

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,9 @@ module Oec2019
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    
+
     config.active_job.queue_adapter = :sidekiq
+    config.assets.initialize_on_precompile = false
+
   end
 end
