fd2c1f289bb483c8974de3a650f1442de85b012f
Paul Buzuloiu <pbuzu025@uottawa.ca>
2019-01-19 01:19:22 -0500

    more factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
-require 'factory_bot'
-FactoryBot.find_definitions
+require 'factory_bot_rails'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
